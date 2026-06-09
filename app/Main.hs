9eda668df7ff63c2350018f28cb35f70b450243b
Tommy Bidne <tbidne@protonmail.com>
2026-06-10 10:53:14 +1200

    Test win32 function

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,21 @@
+{-# LANGUAGE CPP #-}
+
 module Main (main) where
 
 import System.Environment qualified as Base
 import System.Process.Environment.OsString qualified as Process
 
+#if WINDOWS
+import System.Win32.Console qualified as Windows
+#endif
+
 main :: IO ()
 main = do
   Base.getArgs >>= p "Base: "
   Process.getArgs >>= p "Process: "
+#if WINDOWS
+  Windows.getArgs >>= p "Win32 getArgs: "
+  Windows.getArgsNoExe >>= p "Win32 getArgsNoExe: "
+#endif
   where
     p h = putStrLn . (h ++) . show
