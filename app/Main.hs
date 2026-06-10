8849e7a51cfc105d8fa36df3ecff3fe157b0e61f
Tommy Bidne <tbidne@protonmail.com>
2026-06-11 11:52:08 +1200

    Use new getArgsStripped

--- app/Main.hs
+++ app/Main.hs
@@ -2,20 +2,27 @@
 
 module Main (main) where
 
+import Control.Monad ((<=<))
+import Data.List qualified as L
 import System.Environment qualified as Base
+import System.OsString qualified as OsStr
 import System.Process.Environment.OsString qualified as Process
 
 #if WINDOWS
-import System.Win32.Console qualified as Windows
+import System.OsString.Internal.Types (OsString(OsString))
+import System.Win32.WindowsString.Console qualified as Windows
 #endif
 
 main :: IO ()
 main = do
-  Base.getArgs >>= p "Base: "
-  Process.getArgs >>= p "Process: "
+  Base.getArgs >>= p pure "Base: "
+  Process.getArgs >>= p OsStr.decodeUtf "Process: "
 #if WINDOWS
-  Windows.getArgs >>= p "Win32 getArgs: "
-  Windows.getArgsNoExe >>= p "Win32 getArgsNoExe: "
+  let q = OsStr.decodeUtf . OsString
+  Windows.getArgs >>= p q "Win32 getArgs: "
+  Windows.getArgsNoExe >>= p q "Win32 getArgsNoExe: "
+  Windows.getArgsStripped >>= p q "Win32 getArgsStripped: "
 #endif
   where
-    p h = putStrLn . (h ++) . show
+    p :: (a -> IO String) -> String -> [a] -> IO ()
+    p toStr h = (putStrLn . (h ++) . L.intercalate ", ") <=< traverse toStr
